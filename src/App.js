9783543d24b1dd5aa628a32685a60bc222864269
Valentin Barit <valentin.barit@gmail.com>
2022-03-07 16:52:45 +0100

    --wip-- [skip ci]

--- src/App.js
+++ src/App.js
@@ -13,8 +13,6 @@ export default function App() {
   );
 }
 
-registerRootComponent(App);
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -23,3 +21,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+registerRootComponent(App);
--- src/screens/Identification.js
+++ src/screens/Identification.js
@@ -9,16 +9,58 @@ import {
 } from "react-native";
 
 import app from "../../app.json";
+import data from "../../assets/data.json";
 import Button from "../components/Button";
+import Avatar from "../components/Avatar";
 
 function Identification() {
   const [value, setValue] = useState("");
   const [member, setMember] = useState(null);
   const [error, setError] = useState(false);
   const onChange = (text) => {
+    setError(false);
+    setMember(null);
     setValue(text);
   };
-  const onPress = () => {};
+  const onPress = () => {
+    const found = data.members.find(({ lastname, firstname }) =>
+      value.match(
+        new RegExp(`(${firstname} ${lastname})|(${lastname} ${firstname})`, "i")
+      )
+    );
+    setMember(found);
+    setError(!found);
+  };
+  if (member) {
+    return (
+      <View style={styles.root}>
+        <Text style={styles.title}>{app.expo.name}</Text>
+        <Image source={require("../../assets/icon.png")} style={styles.logo} />
+        <Avatar label={member.firstname?.[0]} color={member.favoriteColor} />
+        <Text>
+          Bienvenu·e {member.firstname} {member.lastname} !
+        </Text>
+      </View>
+    );
+  }
+  if (error) {
+    return (
+      <View style={styles.root}>
+        <Text style={styles.title}>{app.expo.name}</Text>
+        <Image source={require("../../assets/icon.png")} style={styles.logo} />
+        <View>
+          <TextInput
+            placeholder="Identifiant"
+            style={styles.input}
+            value={value}
+            onChangeText={onChange}
+          />
+          <Text>Désolé, tu n'es pas enregistré·e.</Text>
+        </View>
+        <Button title="S'enregistrer" />
+      </View>
+    );
+  }
   return (
     <View style={styles.root}>
       <Text style={styles.title}>{app.expo.name}</Text>
