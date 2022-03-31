3b7f34dd3ce16883853303d1932ee9e2a7edbfc3
DFONSECA <dorian.fonseca@ynov.com>
2022-03-31 09:19:26 +0200

    navigation

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,70 @@
-import { View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import Members from "./Members";
+
+const Tab = createBottomTabNavigator();
+
+function Projects() {
+  return (
+    <View style={styles.content}>
+      <Text>Pas de projet.</Text>
+    </View>
+  );
+}
 
 function Home() {
-  return <View />;
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarLabelStyle: styles.tabLabel,
+        tabBarActiveTintColor: "#087f5b",
+        headerTitleStyle: styles.title,
+      }}
+    >
+      <Tab.Screen
+        name="Projets"
+        component={Projects}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="briefcase-account"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Membres"
+        component={Members}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="account-multiple"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontWeight: "700",
+  },
+  tabLabel: {
+    fontSize: 16,
+    fontWeight: "700",
+  },
+  content: {
+    flexGrow: 1,
+    padding: 16,
+  },
+});
 
 export default Home;
