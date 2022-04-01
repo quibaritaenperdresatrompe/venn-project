28fb7214505b255abbe95966ff1b4cb6b3a8d2c9
Valentin Barit <valentin.barit@gmail.com>
2022-04-01 15:01:32 +0200

    feat: 🎸 fetch projects from API REST with React Query

--- src/App.js
+++ src/App.js
@@ -2,24 +2,28 @@ import { useState } from "react";
 import { registerRootComponent } from "expo";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import Identification from "./screens/Identification";
 import Home from "./screens/Home";
 import ColorContext from "./ColorContext";
 
 const Stack = createNativeStackNavigator();
+const queryClient = new QueryClient();
 
 export default function App() {
   const [color, setColor] = useState(null);
   return (
-    <ColorContext.Provider value={[color, setColor]}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Identification" component={Identification} />
-          <Stack.Screen name="Accueil" component={Home} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </ColorContext.Provider>
+    <QueryClientProvider client={queryClient}>
+      <ColorContext.Provider value={[color, setColor]}>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Identification" component={Identification} />
+            <Stack.Screen name="Accueil" component={Home} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ColorContext.Provider>
+    </QueryClientProvider>
   );
 }
 
--- src/screens/Projects.js
+++ src/screens/Projects.js
@@ -1,9 +1,29 @@
 import { View, Text, StyleSheet } from "react-native";
+import { useQuery } from "react-query";
+
+import { getAll } from "../firestore";
 
 function Projects() {
+  const query = useQuery("projects", getAll("projects"));
+  if (query.isLoading) {
+    return (
+      <View style={styles.root}>
+        <Text>Chargement...</Text>
+      </View>
+    );
+  }
+  if (!query.data?.length > 0) {
+    return (
+      <View style={styles.root}>
+        <Text>Pas de projet.</Text>
+      </View>
+    );
+  }
   return (
     <View style={styles.root}>
-      <Text>Pas de projet.</Text>
+      {query.data.map((project) => (
+        <Text key={project.id}>{project.title}</Text>
+      ))}
     </View>
   );
 }
