9cc0b31ce6ec353543d3320793e6fde797772c98
Valentin Barit <valentin.barit@gmail.com>
2022-04-01 14:43:00 +0200

    feat: 🎸 drawer nav

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import { registerRootComponent } from "expo";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Identification from "./screens/Identification";
+import Home from "./screens/Home";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Identification />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Identification" component={Identification} />
+        <Stack.Screen name="Accueil" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: 24,
-  },
-});
 
 registerRootComponent(App);
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,25 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import Members from "./Members";
+
+const Drawer = createDrawerNavigator();
+
+function Projects() {
+  return (
+    <View>
+      <Text>No project yet.</Text>
+    </View>
+  );
+}
 
 function Home() {
-  return <View />;
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Projects" component={Projects} />
+      <Drawer.Screen name="Members" component={Members} />
+    </Drawer.Navigator>
+  );
 }
 
 export default Home;
