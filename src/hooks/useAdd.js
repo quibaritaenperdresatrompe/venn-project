f0b2d2209dae557183a8242224c0137bb146d819
Valentin Barit <valentin.barit@gmail.com>
2022-05-12 07:38:24 +0200

    fix: 🐛 read payload async

--- src/hooks/useAdd.js
+++ src/hooks/useAdd.js
@@ -3,23 +3,26 @@ import { useState, useCallback } from "react";
 
 import { db } from "../firebase";
 
-export default function useAdd(name, payload) {
+export default function useAdd(name) {
   const [loading, setLoading] = useState(false);
   const [id, setId] = useState(null);
   const [error, setError] = useState(false);
-  const add = useCallback(async () => {
-    try {
-      setLoading(true);
-      const snapshot = await addDoc(collection(db, name), {
-        ...payload,
-        createdAt: Date.now(),
-      });
-      setId(snapshot.id);
-    } catch (e) {
-      setError(e);
-    } finally {
-      setLoading(false);
-    }
-  }, [name, payload]);
+  const add = useCallback(
+    async (payload) => {
+      try {
+        setLoading(true);
+        const snapshot = await addDoc(collection(db, name), {
+          ...payload,
+          createdAt: Date.now(),
+        });
+        setId(snapshot.id);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [name]
+  );
   return { loading, id, error, add };
 }
